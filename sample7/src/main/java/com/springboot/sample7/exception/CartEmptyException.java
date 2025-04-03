a66234b479626d7f0ccf3604623d2074bb8ae98b
임현욱 <163104715+limhu02@users.noreply.github.com>
2025-04-03 18:40:14 +0900

    Delete sample7 directory

--- sample7/src/main/java/com/springboot/sample7/exception/CartEmptyException.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.sample7.exception;
-
-public class CartEmptyException extends RuntimeException {
-	public CartEmptyException(String msg) {
-		super(msg);
-	}//생성자
-}
--- sample7/src/main/java/com/springboot/sample7/exception/LoginRequiredException.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.sample7.exception;
-
-public class LoginRequiredException extends RuntimeException {
-	public LoginRequiredException(String msg) {
-		super(msg);
-	}
-}
