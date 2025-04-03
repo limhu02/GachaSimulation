a07b1e32bd95ee970c2f2eed61ea605e7393b3de
임현욱 <163104715+limhu02@users.noreply.github.com>
2025-04-03 18:38:38 +0900

    Delete orm3 directory

--- orm3/src/main/java/com/springboot/orm/dao/ImageDao.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.orm.dao;
-
-import com.springboot.orm.model.Imagebbs_tbl;
-
-public interface ImageDao {
-	Imagebbs_tbl getImagebbs(Integer id);
-	//글번호(id)로 이미지 게시글 조회 
-}
--- orm3/src/main/java/com/springboot/orm/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.orm.dao;
-
-import java.util.List;
-
-public interface UserDao {
-	List<Object[]> getImageList(String id);
-	//계정을 조건으로 이미지 게시글 목록을 검색
-}
